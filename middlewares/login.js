72e8eb4407aa0c926d96808d5639761f1dbf9a0b
aseme <aseme@ASEM>
2023-06-17 17:46:05 +0300

    new

--- middlewares/login.js
+++ middlewares/login.js
@@ -6,69 +6,72 @@ res.clearCookie('refreash');
 }
  
 exports.jwtAuth = async (req, res, next) => {
-  const refreash = req.cookies.refreash;
-  const token = req.cookies.token;
-  if (refreash && token) {
-    let userData;
-    jwt.verify(token, process.env.MY_SECRET, (err, user) => {
-      if (err) {
-        clearCookie(res);
-        next(err);
-      } 
-      userData=user;
-    });
-    
-    jwt.verify(refreash, process.env.REFREASH, (err, user) => {
-      if (err) {
-        clearCookie(res);
-        next(err);
-      }
-    });
-    req.user = userData;
-    next()
-  } 
-  else if (refreash) {
-    //verifying refreash token
-    jwt.verify(refreash, process.env.REFREASH, async (err, user) => {
-      if (err) {
-        clearCookie(res);
-        next(err);
-      } else {
-        const u = await User.findById(user.id);
-        let info;
-        if(u){  
-          info = {
-            name: u.name,
-            role: u.role,
-            courses: u.courses,
-          };
-        }else{
-          next()
-        }
-        //generating access token
-        let accessToken = jwt.sign(info, process.env.MY_SECRET);
 
-        res.cookie('token', accessToken, {
-          httpOnly: true,
-          expires: new Date(Date.now() + 2 * 60 * 60 * 1000),
+      const refreash = req.cookies.refreash;
+      const token = req.cookies.token;
+      if (refreash && token) {
+        let userData;
+        jwt.verify(token, process.env.MY_SECRET, (err, user) => {
+          if (err) {
+            clearCookie(res);
+            req.error=err
+            next();
+          } 
+          userData=user;
         });
-        req.user = info;
-        next();
-      }
-    });
-  } else if (token) {
-    jwt.verify(token, process.env.MY_SECRET, (err, user) => {
-      if (err) {
-        clearCookie(res);
-        next(err);
+        
+        jwt.verify(refreash, process.env.REFREASH, (err, user) => {
+          if (err) {
+            clearCookie(res);
+            next();
+          }
+        });
+        req.user = userData;
+        next()
+      } 
+      else if (refreash) {
+        //verifying refreash token
+        jwt.verify(refreash, process.env.REFREASH, async (err, user) => {
+          if (err) {
+            clearCookie(res);
+            next();
+          } 
+          else {
+              const u = await User.findById(user.id);
+              let info;
+              if(u){  
+                info = {
+                  name: u.name,
+                  role: u.role,
+                  courses: u.courses,
+                };
+            }else{
+                next()
+            }
+            //generating access token
+            let accessToken = jwt.sign(info, process.env.MY_SECRET);
+
+            res.cookie('token', accessToken, {
+              httpOnly: true,
+              expires: new Date(Date.now() + 2 * 60 * 60 * 1000),
+            });
+            req.user = info;
+            next();
+          }
+        });
+      } else if (token) {
+        jwt.verify(token, process.env.MY_SECRET, (err, user) => {
+          if (err) {
+            clearCookie(res);
+            next();
+          } else {
+            req.user = user;
+            next();
+          }
+        });
       } else {
-        req.user = user;
         next();
       }
-    });
-  } else {
-    next();
-  }
 };
 
  
@@ -77,6 +80,7 @@ exports.checkuser=(req,res,next)=>{
     if(token){
       return next()
     }   
+    
     return res.redirect('/login')
 }
 
