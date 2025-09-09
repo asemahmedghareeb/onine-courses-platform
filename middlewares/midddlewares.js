1bda12804419eb7ac0c97dce2aff73269afe8220
asemahmedghareeb <109693344+asemahmedghareeb@users.noreply.github.com>
2025-09-09 16:12:41 +0300

    new

--- middlewares/midddlewares.js
+++ middlewares/midddlewares.js
@@ -105,7 +105,8 @@ exports.adminAndUser = (req, res, next) => {
 
 exports.freeLessonOrCouseSubscriber = (req, res, next) => {
   const lesson = Lesson.findById(req.params.id);
-  const course = Course.findById(lesson.course);
+  const course = Course.findById(lesson.course.toString());
+  console.log(course.title)
   console.log(req.user);
   // const user=User.findById(req.user.id)
   if (lesson.public) {
