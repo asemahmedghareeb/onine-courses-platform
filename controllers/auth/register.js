c919c4ae6fd11f2eee1458da3554c072297fbb37
asemahmedghareeb <109693344+asemahmedghareeb@users.noreply.github.com>
2025-09-11 15:52:53 +0300

    new

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/user");
+const { checkuser } = require("../../middlewares/midddlewares");
 router.get("/", (req, res) => {
-  if (req.user) {
+  const token = req.cookies.token;
+  if (token || req.user) {
     res.render("Error.ejs", { error: " لديك حساب بالفعل" });
   } else {
     res.render("auth/register.ejs", { error: "" });
   }
 });
 
-module.exports = router; 
+module.exports = router;
