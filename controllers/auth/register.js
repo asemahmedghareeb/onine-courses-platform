c2bb983cb213d65433b347b900a0ab19cf5dec46
asemahmedghareeb <109693344+asemahmedghareeb@users.noreply.github.com>
2025-09-11 15:15:35 +0300

    new

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,6 +2,11 @@ const express=require('express')
 const router = express.Router();
 const User=require('../../models/user')
 router.get('/',(req,res)=>{
-  res.render('auth/register.ejs')
+  if(req.user){
+    res.render("Error.ejs",{error:"لقد قمت بتسجيل الدخول"})
+  }
+  else{
+    res.render('auth/register.ejs')
+  }
 })
 module.exports=router
