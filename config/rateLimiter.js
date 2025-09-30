9b7c44f1d319e9a4a6b283440d049775884df2ef
Asem <asemelbadahy@gmail.com>
2025-09-30 16:07:53 +0300

    update[channging authRateLimitter limit]

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -2,18 +2,18 @@ const rateLimit = require('express-rate-limit');
 
 // Apply rate limiting to all requests
 const globalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 1000, // Limit each IP to 100 requests per windowMs
-    message: 'Too many requests from this IP, please try again after 15 minutes',
+    message: 'Too many requests from this IP, please try again after 5 minutes',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 // More aggressive rate limiting for auth routes
 const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 20, // Limit each IP to 20 requests per windowMs
-    message: 'Too many login attempts, please try again after 15 minutes',
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 50, 
+    message: 'Too many login attempts, please try again after 5 minutes',
     standardHeaders: true,
     legacyHeaders: false,
 });
